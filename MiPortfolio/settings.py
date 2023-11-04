a84be88e9a22612e87048546612343460838c1df
Rosalía Lotierzo <84238521+Ro07-r@users.noreply.github.com>
2023-11-04 11:05:26 -0300

    Update settings.py

--- MiPortfolio/settings.py
+++ MiPortfolio/settings.py
@@ -127,5 +127,5 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'MiPortfolio/static'),)
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com" # Parametros del correo Gmail
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "**********************"
-EMAIL_HOST_PASSWORD = "*******************"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
